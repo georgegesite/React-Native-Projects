b31ba47a21ef7fe06aaa439c13d384cd1406bbe2
George Gesite <georgesite33@gmail.com>
2024-04-21 15:14:27 +0800

    able to run on web

--- firstProjects/App.js
+++ firstProjects/App.js
@@ -5,13 +5,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World</Text>
+      <Text>Welcome Developers!</Text>
       <Text>This is my first React-Native App</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+// npm run web: to run on web
+// npm run android: to run on android
+// npx expo start: to run on expo go
 const styles = StyleSheet.create({
   container: {
     flex: 1,
